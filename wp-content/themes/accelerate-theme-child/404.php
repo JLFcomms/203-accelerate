faabc4f8c2b64ba0f8058ad4adcd3ffab7ffcd7a
janel_f <janelforsythe@outlook.com>
2020-05-26 08:06:13 -0400

    recent changes

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the 404 page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+    <div class="page-not-found">
+        <h1>
+             This is somewhat embarrassing, isn't it?
+        </h1>
+        <iframe src="https://giphy.com/embed/14uQ3cOFteDaU" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/404-14uQ3cOFteDaU">via GIPHY</a></p>
+    </div>
+
+<?php get_footer(); ?>
